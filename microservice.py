2dccf8a9ddbf2750124175b46f5390bcfea67e02
Cheath2 <137836013+Cheath2@users.noreply.github.com>
2024-08-03 20:57:05 -0400

    building
    building book submission

--- microservice.py
+++ microservice.py
@@ -1,4 +1,4 @@
-# Book submission Microservice
+# Book submission Microservice using REQ/Reply in ZMQ
 
 import zmq
 import json
@@ -6,5 +6,24 @@ import os
 
 
 context = zmq.Context()
-
 socket = context.socket(zmq.REP)
+
+# Change socket
+socket.bind("tcp:*:5555")
+
+
+
+def add_book(user_request):
+    try:
+        username = user_request['username']
+        new_books = user_request['books']
+
+        book_data = load_data()
+
+        if username in book_data:
+            user_books = book_data[username]
+        else:
+            user_books = []
+
+
+    except:
