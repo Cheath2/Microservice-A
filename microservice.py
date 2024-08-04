1d9016f21c75b77bbdbcc76b7d86a990871bdc4c
Cheath2 <137836013+Cheath2@users.noreply.github.com>
2024-08-03 23:28:12 -0400

    limit 3
    limited to 3 per user , added get list function

--- microservice.py
+++ microservice.py
@@ -1,28 +1,30 @@
 # Book submission Microservice using REQ/Reply in ZMQ
 
+# imports
 import zmq
 import json
 import os
 
-
+# ZMQ Port
 context = zmq.Context()
 socket = context.socket(zmq.REP)
 
 # Change socket as needed
-socket.bind("tcp:*:5555")
+socket.bind("tcp://localhost:5555")
 
+# Assign data file to variable
 DATA_FILE = 'bookdata.json'
 
 
 def load_data():
-    '''Function to present book data  '''
+    '''Function to present/load book data  '''
     if os.path.exists(DATA_FILE):
         with open(DATA_FILE, 'r') as file:
             return json.load(file)
     return{}
 
 
-def save_data():
+def save_data(data):
     ''' Function to write data to bookdata.json'''
     with open(DATA_FILE, 'w') as file:
         json.dump(data, file, indent=4)
@@ -56,9 +58,43 @@ def add_book(user_request):
     # exception
     except Exception as e:
         return {"status":"error", "message":  f"An error has occurred: {e}."}
+    
+
+
+
+def get_books(user_request):
+    try:
+
+        username = user_request['username']
+        book_data = load_data()
+
+        if username in book_data:
+            user_books = book_data[username]
+            return {'status': "success", "books": user_books}
+        else:
+            return {"status": "error", "message": "No books found."}
+        
+    except Exception as e:
+        return{"status": "error", "message": f"Error {e}."}
+    
+
 
 # Await request, respond as needed
 while True:
     user_request = socket.recv_json()
-    response = add_book(user_request)
+
+
+    if user_request.get('action') == 'submit_books':
+        response = add_book(user_request)
+    
+    elif user_request.get('action') == 'get_list':
+        response = get_books(user_request)
+    
+    else:
+
+        response = add_book(user_request)
+
+
+
+    # ZMQ Actions 
     socket.send_json(response)
--- readme.txt
+++ readme.txt
@@ -2,6 +2,12 @@ Book submission Microservice
 
 Communication Pipe: ZMQ
 
+Currently running on local port
+
+    Actions:
+     'submit_books' to submit new books
+     'get_list' to get titles in user's data.
+
 This Microservice uses a REQ-Reply ZMQ pattern to offer funcationality to update book information (title,author, url, and description) by username. A user can submit up to three books
  
 
