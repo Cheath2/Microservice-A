186391d2aaea5463138a50bd37d6689aab496101
Cheath2 <137836013+Cheath2@users.noreply.github.com>
2024-08-03 21:10:29 -0400

    Update microservice.py

--- microservice.py
+++ microservice.py
@@ -11,22 +11,37 @@ socket = context.socket(zmq.REP)
 # Change socket
 socket.bind("tcp:*:5555")
 
+DATA_FILE = 'bookdata.json'
 
+
+def load data():
+
+
+def save data():
 
 def add_book(user_request):
+
     try:
         username = user_request['username']
         new_books = user_request['books']
 
         book_data = load_data()
 
+        # check for existing user, present current list
         if username in book_data:
             user_books = book_data[username]
+        # If no books found for user, extend list to add new book(s)
         else:
             user_books = []
+
+        user_books.extend(new_books)
+        books_data[username] = user_books
+
+
+        save_data(book_data)
 
 
 
     # exception
     except: Exception as e:
-        return {error}
+    return {error}
